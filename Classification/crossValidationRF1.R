aa52b7e0003ba48873222e380de6e7b8974f7ade
pharika31 <pharika31@gmail.com>
2017-04-04 16:21:03 -0400

    first commit

---
+++ Classification/crossValidationRF1.R
@@ -0,0 +1,158 @@
+
+library(gdata)
+library(e1071)
+library(randomForest)
+library(mlbench)
+library(caret)
+library(Boruta)
+library(psych)
+library(limma)
+library(graph)
+library(RBGL)
+library(PerfMeas)
+library(varSelRF)
+
+show('File name is : crossValidationRF1.R - Choose this File from Stored Folder Path')
+filename = "crossValidationRF1.R"
+filepath=1
+filepath = file.choose()
+if(filepath!= 1){
+  dir = substr(filepath, 1, nchar(filepath)-nchar(filename))
+  dir
+  setwd(dir)
+  
+
+
+
+
+
+rawData=read.csv("trainData1.csv",header=FALSE)
+rawDataLabel=read.csv("trainLabel1.csv",header=FALSE)[,1]
+
+
+
+
+#pre-processing : removing columns with 0 Standard Deviation
+preProcRawData = preProcess(rawData, method=c("zv","nzv","range"))
+rawData = predict(preProcRawData,rawData)
+#sdVar=lapply(rawData, sd)
+#if(length(sdvar)>0){
+#lowSdIdx = which(unlist(sdVar)==0)
+#rawData = rawData[,-lowSdIdx]
+#}
+#Generate random indices to shuffle the samples
+#rawDataLabel=as.factor(rawDataLabel[,1])
+
+boruta.train <- Boruta(x=rawData, y=as.factor(rawDataLabel), doTrace = 2)
+
+lz<-lapply(1:ncol(boruta.train$ImpHistory),function(i)
+  boruta.train$ImpHistory[is.finite(boruta.train$ImpHistory[,i]),i])
+names(lz) <- colnames(boruta.train$ImpHistory)
+Labels <- sort(sapply(lz,median))
+#axis(side = 1,las=2,labels = names(Labels),
+     #at = 1:ncol(boruta.train$ImpHistory), cex.axis = 0.7)
+
+final.boruta <- TentativeRoughFix(boruta.train)
+
+
+rawData <- rawData[,getSelectedAttributes(final.boruta, withTentative = F)]
+
+
+
+#1st try gridDF = expand.grid(.mtry=c(2^seq(1,12)),ntree=c(100,500,1000))
+#2nd try  512,600,650,700,800,900,1024
+gridDF = expand.grid(.mtry=c(15),ntree=c(1000,100))
+
+
+
+#function for RF with cross-validation
+crossVal = function(rawData, rawDataLabel,bkp_rawData=rawData){
+
+
+completeSet =sample(seq(1,nrow(rawData)), nrow(rawData),replace=FALSE)
+
+#create k folds
+k_folds= 2
+flds = createFolds(completeSet, k_folds, list = TRUE, returnTrain = FALSE)
+
+model_list=list()
+fold_preds=list()
+selected_vars=list()
+cv_auc = c()
+sd_auc = c()
+count=1
+#rawDataLabel=as.factor(rawDataLabel[,1])
+uniqueLabel = as.numeric(unique(rawDataLabel))
+  for(i in 1:nrow(gridDF)){
+   
+    error_var=c()    
+      for(j in 1:k_folds){   
+    
+      fold_model = randomForest(x=rawData[unlist(flds[-j]),] ,y=as.factor(rawDataLabel[unlist(flds[-j])]), mtry=gridDF$.mtry[i],ntree=gridDF$ntree[i],importance=TRUE)
+      #show(fold_model)
+      fold_preds = as.numeric(predict(fold_model,rawData[unlist(flds[j]),]))
+      #show(fold_preds)
+      true_values = as.numeric(rawDataLabel[unlist(flds[j])])
+      
+      
+      predicted_matrix=dummy.code(fold_preds)
+      if(length(setdiff(uniqueLabel, unique(fold_preds))) > 0){
+        missingLabels = setdiff(uniqueLabel, unique(fold_preds))
+          insertCols = matrix(0,nrow(predicted_matrix),length(missingLabels))    
+          colnames(insertCols) = missingLabels
+          predicted_matrix = cbind(predicted_matrix,insertCols)
+       }
+       predicted_matrix = predicted_matrix[,as.character(sort(uniqueLabel))]
+      true_matrix=dummy.code(true_values)
+      #handle condition - if any class is not present in predictions or true values, we are including that column with 0s
+
+      if(length(setdiff(uniqueLabel, unique(true_values))) > 0){
+        missingLabels = setdiff(uniqueLabel, unique(true_values))
+          insertCols = matrix(0,nrow(true_matrix),length(missingLabels))    
+          colnames(insertCols) = missingLabels
+          true_matrix = cbind(true_matrix,insertCols)
+       }
+      #error_var = c(error_var,AUC.single(fold_preds1, true_values))
+      error_var = c(error_var,AUC.single.over.classes(true_matrix, predicted_matrix)$average)
+
+      }
+    cv_auc[i] = mean(error_var) 
+    sd_auc[i] = sd(error_var) 
+}
+return (cv_auc)
+}#end of function
+
+#performing repeat CV
+cv_auc_repeat=matrix(0,1,nrow(gridDF))
+
+for (m in 1:100){
+  show(paste("m = " , m) )
+  
+  cv_auc_repeat=rbind(cv_auc_repeat,crossVal(rawData,rawDataLabel,bkp_rawData=rawData))
+}
+cv_auc_repeat=cv_auc_repeat[-1,]
+param_metrics=colMeans(cv_auc_repeat)
+
+
+best_params=gridDF[which.max(param_metrics),]
+show(best_params)
+max(param_metrics)
+
+# //predictions for test data
+best_model = randomForest(x=rawData ,y=as.factor(rawDataLabel), mtry=best_params$.mtry,ntree=best_params$ntree,importance=TRUE)
+
+ testDataRaw = read.csv("testData1.csv",header = FALSE)
+ 
+ #pre-processing : removing columns with 0 Standard Deviation
+ testDataPP = predict(preProcRawData,testDataRaw,type="votes")
+ testDataFS=testDataPP[,getSelectedAttributes(final.boruta, withTentative = F)]
+ testPreds = as.numeric(predict(best_model,testDataFS))
+write.table(testPreds,file="PunyamurthulaClassificationCV1.txt",row.names = FALSE,col.names = FALSE)
+show("Predictions file created. Please check file PunyamurthulaClassificationCV1.txt in the following Path")
+
+show(dir)
+
+}else{
+  show('Please choose the File mentioned : crossValidationRF1.R')
+}
+
---
+++ MissingValue Estimation/RUN_Missing_Value_Estimation_2.R
@@ -0,0 +1,92 @@
+
+library(softImpute)
+
+
+show('File name is : RUN_Missing_Value_Estimation_2.R - Choose this File from Stored Folder Path')
+filename = "RUN_Missing_Value_Estimation_2.R"
+filepath=1
+filepath = file.choose()
+if(filepath!= 1){
+  dir = substr(filepath, 1, nchar(filepath)-nchar(filename))
+  dir
+  setwd(dir)
+
+
+
+rmse <- function(error)
+{
+  sqrt(mean(error^2))
+}
+
+# Function that returns Mean Absolute Error
+mae <- function(error)
+{
+  mean(abs(error))
+}
+
+
+rawData = read.csv("MissingData2.csv",header=FALSE)
+rawData[rawData==1.000e+99]<-NA
+actualDataMadeMissing=data.matrix(rawData)
+
+rawDataM = data.matrix(rawData)
+notNA=which(!is.na(rawDataM))
+inputWithMissing = sample(notNA,0.04*length(notNA),replace=FALSE)
+rawDataM[inputWithMissing]<-NA 
+
+min(dim(actualDataMadeMissing))-1
+
+gridDF = expand.grid(lam=c(0,2),rank=c(20))
+
+errorList = c()
+for(i in 1:nrow(gridDF)){
+  
+  softImputedValues = softImpute(rawDataM,lambda=gridDF$lam[i],rank.max = gridDF$rank[i])
+  
+  umat=((softImputedValues["u"][[1]]))
+  dmat =((softImputedValues["d"][[1]]))
+  vmat = ((softImputedValues["v"][[1]]))
+  
+  diagD=matrix(0,ncol(umat),ncol(umat))
+  diag(diagD) = c(dmat)
+  
+  imputedFinal = umat %*% diagD %*% t(vmat)
+  
+  missingImputedFinal=rawDataM
+  missingImputedFinal[ which(is.na(rawDataM))]= imputedFinal[ which(is.na(rawDataM))]
+  
+  result= missingImputedFinal
+  
+  actual = actualDataMadeMissing[inputWithMissing]
+  predicted = result[inputWithMissing]
+  error <- actual - predicted
+  errorList[i]=rmse(error)
+  
+  
+}
+
+best_param_in = (which(errorList==min(errorList)))
+
+
+softImputedValues = softImpute(actualDataMadeMissing,lambda=gridDF$lam[best_param_in],rank.max = gridDF$lam[best_param_in])
+umat=((softImputedValues["u"][[1]]))
+dmat =((softImputedValues["d"][[1]]))
+vmat = ((softImputedValues["v"][[1]]))
+
+diagD=matrix(0,ncol(umat),ncol(umat))
+diag(diagD) = c(dmat)
+
+imputedFinal = umat %*% diagD %*% t(vmat)
+missingImputedFinal = actualDataMadeMissing
+missingImputedFinal[ which(is.na(actualDataMadeMissing))]= imputedFinal[which(is.na(actualDataMadeMissing))]
+
+write.table(missingImputedFinal, file="PunyamurthulaMissingResult2.txt",row.names = FALSE,col.names = FALSE,sep="\t")
+
+
+show("Predictions file created. Please check file PunyamurthulaMissingResult2.txt in the following Path")
+
+show(dir)
+
+}else{
+  show('Please choose the File mentioned : RUN_Missing_Value_Estimation_2.R')
+}
